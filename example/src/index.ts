bbfce7086a65843df06546bd6675b0ec572709fe
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-09-03 18:10:48 +0200

    feat: update example to use Prisma 6

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { existsFn } from "../dist"
+import { PrismaClient } from "./generated/prisma/client";
+import { existsFn } from "../../dist"
 
 const prisma = new PrismaClient().$extends(existsFn({}))
 
